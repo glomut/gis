0e960b96977890fb2335d218fe201f4c47df840a
Gloria mutunga <glomut@gmail.com>
2020-05-25 12:47:59 +0300

    updated application form to use wizzard

--- sponsorship/urls.py
+++ sponsorship/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('register/applicant/', user_views.register_applicant, name='register-applicant'),
     path('register/sponsor/', user_views.register_sponsor, name='register-sponsor'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='main/home.html'), name='logout'),
 ]
 
 if settings.DEBUG:
